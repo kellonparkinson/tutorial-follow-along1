861e716b6c35a2e165390f910ed0b9139177b372
kellonparkinson <kellon.parkinson@gmail.com>
2022-04-27 18:30:47 -0600

    added a button with function to Home

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,13 @@
 const Home = () => {
+
+    const buttonClick = (name) => {
+        console.log('Welcome home, ' + name);
+    }
+
     return (  
         <div className="home">
             <h2>Homepage</h2>
+            <button onClick={() => buttonClick('Kellon')}>Click Me</button>
         </div>
     );
 }
