b3a757aac5e430cb977b36d4cebe0fed6beeb3c3
kellonparkinson <kellon.parkinson@gmail.com>
2022-06-07 16:55:00 -0600

    keeping code recent

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,7 +1,5 @@
-const BlogList = (props) => {
+const BlogList = ({ blogs, title }) => {
 
-    const blogs = props.blogs;
-    const title = props.title;
 
     return (  
         <div className="blog-list">
